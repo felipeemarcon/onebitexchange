85a3276eab1e11545da88536ee5829335869b8c7
Felipe Marcon <felipeemarcon@gmail.com>
2018-12-17 12:50:01 -0200

    Index page and coffee script

--- spec/system/exchanges_index_system_spec.rb
+++ spec/system/exchanges_index_system_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe "Exchange Currency Process", :type => :system, js: true do
     within("#exchange_form") do 
       select('EUR', from: 'source_currency')
       select('USD', from: 'target_currency')
-      fill_in 'amount', with: '10'
+      fill_in 'amount', with: rand(1..9999)
     end
     
     click_button 'CONVERTER'
 
     # save_and_open_page
-    exepect(page).to have_content("value")
+    expect(page).to have_content("value")
  end
 end
