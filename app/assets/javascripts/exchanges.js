c6039a5f3597382b73cfd829d02c63a2d0f1caef
Felipe Marcon <felipeemarcon@gmail.com>
2019-02-19 19:32:57 -0300

    Change to english and fix api

--- app/assets/javascripts/exchanges.js
+++ app/assets/javascripts/exchanges.js
@@ -1,5 +1,4 @@
 $(document).ready(() => {
-  
   selectCustom();
   focusExchanged();
   sendForm();
@@ -7,49 +6,49 @@ $(document).ready(() => {
   // invertCurrency();
 
   function selectCustom() {
-    $('.select-custom-exchange').customSelect();
+    $(".select-custom-exchange").customSelect();
   }
 
   function focusExchanged() {
-    const btnFocus = $('#focus-formExchange');
-    const resetButton = $('.reset_form')
-    let amount = $('#amount')
+    const btnFocus = $("#focus-formExchange");
+    const resetButton = $(".reset_form");
+    let amount = $("#amount");
 
-    btnFocus.click((e) => {
+    btnFocus.click(e => {
       e.preventDefault();
       amount.focus();
     });
 
-    resetButton.click((e) => {
+    resetButton.click(e => {
       amount.focus();
     });
   }
 
   function sendForm() {
-    const exchangeForm = $('#exchange_form');
+    const exchangeForm = $("#exchange_form");
 
-    $('#exchange_form input, #exchange_form select').on('input', function () {
+    $("#exchange_form input, #exchange_form select").on("input", function() {
       exchangeForm.submit();
-    });    
+    });
   }
 
   function calculateCurrency() {
-    const exchangeForm = $('#exchange_form');
+    const exchangeForm = $("#exchange_form");
 
     exchangeForm.submit(() => {
-      const formAction = exchangeForm.attr('action');
+      const formAction = exchangeForm.attr("action");
 
-      let convertRoute = '/convert';
-      let sourceCurrency = $('#source_currency');
-      let targetCurrency = $('#target_currency');
-      let amount = $('#amount');
-      let convertResult = $('#resultExchange');
-      let loading = $('.app__form--loading');
+      let convertRoute = "/convert";
+      let sourceCurrency = $("#source_currency");
+      let targetCurrency = $("#target_currency");
+      let amount = $("#amount");
+      let convertResult = $("#resultExchange");
+      let loading = $(".app__form--loading");
 
       if (formAction === convertRoute) {
         $.ajax(convertRoute, {
-          type: 'GET',
-          dataType: 'json',
+          type: "GET",
+          dataType: "json",
           data: {
             source_currency: sourceCurrency.val(),
             target_currency: targetCurrency.val(),
@@ -60,11 +59,13 @@ $(document).ready(() => {
           },
           success(data, text, jqXHR) {
             loading.show();
-            convertResult.css('opacity', '0.2');
+            convertResult.css("opacity", "0.2");
             setTimeout(() => {
-              return convertResult.val(Math.round(data.value * 100) / 100),
-              convertResult.css('opacity', '1'),
-              loading.hide();
+              return (
+                convertResult.val(Math.round(data.value * 100) / 100),
+                convertResult.css("opacity", "1"),
+                loading.hide()
+              );
             }, 1000);
           }
         });
@@ -74,33 +75,37 @@ $(document).ready(() => {
   }
 
   function invertCurrency() {
-
-    let sourceCurrency = $('#source_currency');
+    let sourceCurrency = $("#source_currency");
     let sourceCurrencySelect = sourceCurrency.find(":selected");
 
-    targetCurrency = $('#target_currency');
+    targetCurrency = $("#target_currency");
     targetCurrencySelect = targetCurrency.find(":selected");
 
-    const exchangeForm = $('#exchange_form');
-    const triggerInvert = $('#invertConvert');
+    const exchangeForm = $("#exchange_form");
+    const triggerInvert = $("#invertConvert");
 
     sourceCurrencyVal = sourceCurrencySelect.val();
     targetCurrencyVal = targetCurrencySelect.val();
 
-    triggerInvert.on("click", function (e) {
+    triggerInvert.on("click", function(e) {
       e.preventDefault();
 
       // target
-      $(`#source_currency option[value=${targetCurrencyVal}]`).attr('selected', 'selected');
+      $(`#source_currency option[value=${targetCurrencyVal}]`).attr(
+        "selected",
+        "selected"
+      );
       sourceCurrency.val(targetCurrencyVal);
-      
-      $(`#target_currency option[value=${sourceCurrencyVal}]`).attr('selected', 'selected');
+
+      $(`#target_currency option[value=${sourceCurrencyVal}]`).attr(
+        "selected",
+        "selected"
+      );
 
       // Report results
       console.log(sourceCurrencySelect.val(), targetCurrencySelect.val());
 
       exchangeForm.submit();
-
     });
   }
 });
